a9576a33f1569e5fe09d706b248a4ccc833a873b
Abi Raskin <abiraskin@gmail.com>
2018-08-01 03:37:16 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,6 +4,6 @@ def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     taken = false
   else
-    taken = tru
-end    
+    taken = true
+end
 taken
